adafc714dca15cdcb69ddbeddf9a1cac169ff609
Delwser <87214159+Delwser@users.noreply.github.com>
2024-08-18 00:22:08 -0300

    Exercicios requisitados em sala de aula
    Existe dois scripts. O primeiro faz a leitura da wordlist e no final mostra a o usuário as 10 senhas mais utilizadas. O segundo código ele faz uma requisição em nos 20 primeiros subdominos da wordlist, e da print no status da requisição

---
+++ exercicios_pardal/exercicio2.py
@@ -0,0 +1,16 @@
+import requests
+
+def buscando_urls():
+    with open("subdomains-top1million-5000.txt", "r") as arquivo: #Abrindo o arquivo, e lendo ele.
+        urls = arquivo.readlines() #Definindo quais tipos de 
+        
+        for i in range(0, 20):  # Puxando as primeiras 20 linhas
+            url = "https://" + urls[i].strip()  # Adicionando https para fazer requisição
+            
+            try:
+                response = requests.get(url)  # Fazendo a requisição
+                print(f"O site {url} retornou status: {response.status_code}")
+            except requests.exceptions.ConnectionError:
+                print(f"A requisição falhou para o site {url}")
+
+buscando_urls()
