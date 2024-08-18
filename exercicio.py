c22954dbcfed54bcf5c9657bc220f9089c74accc
Delwser <87214159+Delwser@users.noreply.github.com>
2024-08-18 00:02:56 -0300

    Delete exercicio.py

--- exercicio.py
+++
@@ -1,9 +0,0 @@
-def buscando_senha():
-    with open("darkweb2017-top.txt", "r") as arquivo:
-        alace = arquivo.readlines()
-        for a in range(min(10, len(alace))):
-            print(alace[a])
-        
-
-
-buscando_senha()
